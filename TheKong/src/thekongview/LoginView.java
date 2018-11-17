f535c7fb7ed02372430d34beb48ef95dec98a344
Malcolm R. Jones <maljonesdev@gmail.com>
2018-11-17 14:38:02 -0500

    Add LoginView class

--- TheKong/src/thekongview/LoginView.java
+++ TheKong/src/thekongview/LoginView.java
@@ -7,7 +7,7 @@ import javafx.scene.control.ChoiceDialog;
 public class LoginView extends ChoiceDialog<String> {
         
     public LoginView(List<String> choices) {
-        
+         
     }
     
     public String displayLoginView() {
