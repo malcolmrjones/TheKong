4a4ecd944f07d3f8ecea40f00661707fab4d3eb8
Malcolm R. Jones <maljonesdev@gmail.com>
2018-11-18 16:45:42 -0500

    Restructured GameView

--- TheKong/src/thekongview/GameView.java
+++ TheKong/src/thekongview/GameView.java
@@ -1,6 +1,12 @@
 package thekongview;
 
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 
 public class GameView extends BorderPane {
@@ -11,14 +17,18 @@ public class GameView extends BorderPane {
     
     public GameView() {
         
-        statusview = new StatusView("PLAYERNAME", 100, 200, 1);
-        this.setTop(statusview);
-        
         //playareaview = new PlayAreaView();
         //this.setCenter(playareaview);
         
+        statusview = new StatusView("PLAYERNAME", 100, 200, 1);     
         commandview = new CommandView();
-        this.setBottom(commandview);
+        VBox vboxBottom = new VBox();
+        vboxBottom.getChildren().addAll(statusview, commandview);
+        vboxBottom.setSpacing(10);
+        vboxBottom.setPadding(new Insets(5, 10, 10, 10));
+        vboxBottom.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        
+        this.setBottom(vboxBottom);
     }
     
 }
